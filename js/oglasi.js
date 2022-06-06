cd50af63846e1c89405cb72ff8f0f4ce9b71ba84
DusanTodorovic5 <dtodorovic40@gmail.com>
2022-06-06 18:21:26 +0200

    Dodati oglasi bez js

---
+++ js/oglasi.js
@@ -0,0 +1,7 @@
+var oglasi = [
+    {
+        "naziv" : "Šeki",
+        "kontakt" : "12345678",
+        "opis" : "Pudlica tamno braon boje. Jako je vesela i umiljata. Srednjeg rasta, ima oko 5 kg"
+    }
+];
